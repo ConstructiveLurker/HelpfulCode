781d36e0ee24735f638b0464253a96d66bd0c0d9
Talia Avery <18.talia.avery@ksd.org>
2018-05-07 13:52:37 -0700

    Animation

---
+++ HelpfulCode - Unity/Assets/Scripts/DragonAnimationTransition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonAnimationTransition : MonoBehaviour 
+{
+
+	Animator dragonAnim;
+
+	// Use this for initialization
+	void Start () 
+	{
+		dragonAnim = GetComponent <Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Alpha1)) 
+		{
+			dragonAnim.SetInteger ("AnimationState", 0);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha2)) 
+		{
+			dragonAnim.SetInteger ("AnimationState", 1);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Alpha3)) 
+		{
+			dragonAnim.SetInteger ("AnimationState", 2);
+		}
+	}
+}
